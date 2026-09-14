ad53628fccdf45f0447adba96ea0021e22da0951
Karunakara Rao K <karunakar.kancharla@hotmail.com>
2026-09-14 19:06:51 +0530

    added parallel_dag

---
+++ dags/parallel_dag.py
@@ -0,0 +1,41 @@
+from airflow.sdk import dag, task
+
+@dag
+def parallel_dag():
+
+    @task.bash
+    def task_bash():
+        return 'echo "This is a bash task"'
+
+    @task.python
+    def api_data():
+        data = {"place": "Gudivada", "Count": 3000}
+        return data
+
+    @task.python
+    def db_data():
+        data = {"DB": "SQLServer", "Sql Command": "select * from table"}
+        return data
+
+    @task.python
+    def gcs_data():
+        data = {"Bucket": "GCS", "File": "gs://bucket/file.csv"}
+        return data
+
+    @task.python
+    def process_data(api_data, db_data, gcs_data):
+        print("Processing data from API:", api_data)
+        print("Processing data from DB:", db_data)
+        print("Processing data from GCS:", gcs_data)
+
+
+    task_bash = task_bash()
+    api_data = api_data()
+    db_data = db_data()
+    gcs_data = gcs_data()
+    process_data = process_data(api_data, db_data, gcs_data)
+
+    # Define the task dependencies
+    task_bash >> [api_data, db_data, gcs_data] >> process_data
+
+parallel_dag_instance = parallel_dag()
